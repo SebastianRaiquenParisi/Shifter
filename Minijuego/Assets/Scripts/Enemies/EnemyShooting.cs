157ccf07a2d176ca676e94688cc4ba01beb78030
ParEasy <sraiquenp@gmail.com>
2017-05-27 01:25:55 -0300

    Sprites menos bosses

---
+++ Minijuego/Assets/Scripts/Enemies/EnemyShooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public float fireDelay = 0.5f;
+    float cooldownTimer = 0;
+    public float distanciaDeDisparo = 20;
+
+    Transform player;
+
+    int bulletLayer;
+    private void Start()
+    {
+        bulletLayer = gameObject.layer;
+    }
+    void Update()
+    {
+        if (player == null)
+        {
+            GameObject gObj = GameObject.Find("Player");
+            if (gObj != null)
+            {
+                player = gObj.transform;
+            }
+        }
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer <= 0 && player != null && Vector3.Distance(transform.position, player.position) < distanciaDeDisparo)
+        {
+            //Fire
+            cooldownTimer = fireDelay;
+            Vector3 offset = transform.rotation * new Vector3(0, 1.3f, 0);
+            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+            bulletGO.layer = bulletLayer;
+        }
+    }
+}
---
+++ Minijuego/Assets/Scripts/Enemies/Sword Game/SwordSuperAttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSuperAttack : MonoBehaviour {
+
+    public float tiempoDeAdvertencia = 2;
+    public float tiempoDeDaño = 1.4f;
+
+    // Use this for initialization
+    void Start() {
+        float width = Camera.main.orthographicSize * Screen.width / Screen.height;
+        width = width / 2.6f; 
+        float heigth = Camera.main.orthographicSize / 2.6f;
+        int random = Random.Range(1, 4);
+        switch (random)
+        {
+            case 4:
+                gameObject.transform.localScale = new Vector2(width/2, heigth);
+                gameObject.transform.position = new Vector3(-width * 1.9f, 0, 0);
+                break;
+            case 3:
+                gameObject.transform.localScale = new Vector2(width/2, heigth);
+                gameObject.transform.position = new Vector3(width * 1.9f, 0, 0);
+                break;
+            case 2:
+                gameObject.transform.localScale = new Vector2(width, heigth/2);
+                gameObject.transform.position = new Vector3(0, -heigth * 1.9f, 0);
+                break;
+            case 1:
+                gameObject.transform.localScale = new Vector2(width, heigth/2);
+                gameObject.transform.position = new Vector3(0, heigth * 1.9f, 0);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(tiempoDeAdvertencia > 0)
+        {
+            gameObject.layer = 10;
+        }
+        else
+        {
+            tiempoDeDaño -= Time.deltaTime;
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            gameObject.layer = 8;
+        }
+        tiempoDeAdvertencia -= Time.deltaTime;
+        if(tiempoDeDaño <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
