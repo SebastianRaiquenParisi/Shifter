f7daa74128f7b632da7b959def819cd8e7049bfe
JoacoLerer <joacolerer@gmail.com>
2017-06-01 15:39:05 -0300

    Unity project

---
+++ Shifter/Shifter/Assets/Scripts/Menu/Exit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Exit : MonoBehaviour {
+
+    public GameObject messageBox;
+    public void message() {
+        messageBox.SetActive(true);
+    }
+    public void Yes()
+    {
+        Application.Quit();
+    }
+    public void No()
+    {
+        messageBox.SetActive(false);
+    }
+}
---
+++ Shifter/Shifter/Assets/Scripts/Menu/newGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class newGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start (int level) {
+        SceneManager.LoadScene(level);
+    }
+	
+}
