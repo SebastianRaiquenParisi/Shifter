a4e9fcfee85dc60b48cbc5e688aec55ac9f5c434
ParEasy <sraiquenp@gmail.com>
2017-06-04 17:15:24 -0300

    Faltan sprites de espada giratoria, warning, super ataque de espada

---
+++ DialogueTest/Assets/Scripts/ActivateTextAtLine.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTextAtLine : MonoBehaviour {
+
+    public TextAsset text;
+
+    public int startLine;
+    public int endLine;
+
+    public TextBoxManager textBox;
+
+    public bool destroyWhenActivated;
+
+    public bool requireButtonPressed;
+    private bool waitForPressed;
+
+	// Use this for initialization
+	void Start () {
+        textBox = FindObjectOfType<TextBoxManager>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(waitForPressed && Input.GetKeyDown(KeyCode.E))
+        {
+            textBox.ReloadScript(text);
+            textBox.currentLine = startLine;
+            textBox.endAtLine = endLine;
+            textBox.EnableTextBox();
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player") // "Player" es como se llama el pj obviamente
+        {
+            if (requireButtonPressed)
+            {
+                waitForPressed = true;
+                return;
+            }
+            textBox.ReloadScript(text);
+            textBox.currentLine = startLine;
+            textBox.endAtLine = endLine;
+            textBox.EnableTextBox();
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            waitForPressed = false;
+        }
+    }
+}
---
+++ DialogueTest/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+
+    public Text textUI;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public bool isActive;
+
+    public bool stopPlayerMovement;
+
+    private bool isTyping = false;
+    private bool cancelTyping = false;
+    public float typeSpeed;
+    //Tenes que poner un bool en el player movement script,
+    //en update: if(!canMove){ return; }
+
+    //public Player player, para inhabilitar el movimiento y esa gilada;
+
+    
+	void Start () {
+        // Buscas el player;
+		if(textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+        if(endAtLine == 0)
+        {
+            endAtLine = textLines.Length-1;
+        }
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else
+        {
+            DisableTextBox();
+        }
+	}
+	
+	void Update () {
+        if (!isActive)
+        {
+            return;
+        }
+        //textUI.text = textLines[currentLine];
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!isTyping)
+            {
+                currentLine++;
+                if (currentLine > endAtLine)
+                {
+                    DisableTextBox();
+                }
+                else
+                {
+                    StartCoroutine(TextScroll(textLines[currentLine]));
+                }
+            }
+            else if(isTyping && !cancelTyping)
+            {
+                cancelTyping = true;
+            }
+        }
+       
+	}
+
+    private IEnumerator TextScroll(string lineOfText)
+    {
+        int letter = 0;
+        textUI.text = "";
+        isTyping = true;
+        cancelTyping = false;
+        while (isTyping && !cancelTyping && (letter < lineOfText.Length-1))
+        {
+            textUI.text += lineOfText[letter];
+            letter++;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        textUI.text = lineOfText;
+        isTyping = false;
+        cancelTyping = false;
+    }
+
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+        isActive = true;
+        if (stopPlayerMovement)
+        {
+            //player.canMove = false;
+        }
+        StartCoroutine(TextScroll(textLines[currentLine]));
+    }
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+        isActive = false;
+        //player.canMove = true;
+    }
+
+    public void ReloadScript(TextAsset textAsset)
+    {
+        if(textAsset != null)
+        {
+            textLines = new string[1];
+            textLines = (textAsset.text.Split('\n'));
+        }
+    }
+}
