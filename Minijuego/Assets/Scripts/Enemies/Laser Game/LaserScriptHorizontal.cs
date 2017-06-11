bc43e7bb99235f3f431b834afb3233e10e0fd5df
ParEasy <sraiquenp@gmail.com>
2017-06-11 04:07:15 -0300

    Faltan sprites de espadas

--- Minijuego/Assets/Scripts/Enemies/Laser Game/LaserScriptHorizontal.cs
+++ Minijuego/Assets/Scripts/Enemies/Laser Game/LaserScriptHorizontal.cs
@@ -10,26 +10,33 @@ public class LaserScriptHorizontal : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        float width = Camera.main.orthographicSize * Screen.width / Screen.height;
-        width = width / 2.6f;
-        float heigth = Camera.main.orthographicSize / 2.6f;
-        Vector3 localScale = gameObject.transform.localScale;
-        localScale.x = width * 2;
-        localScale.y = heigth * 0.5f;
-        gameObject.transform.localScale = localScale;
         correctSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
-        
+        gameObject.transform.position = new Vector3(0, gameObject.transform.position.y, gameObject.transform.position.z);
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (tiempoDeAdvertencia > 0)
         {
+            float width = Camera.main.orthographicSize * Screen.width / Screen.height;
+            width = width / 2.6f;
+            float heigth = Camera.main.orthographicSize / 2.6f;
+            Vector3 localScale = gameObject.transform.localScale;
+            localScale.x = width * 0.1f;
+            localScale.y = heigth * 0.12f;
+            gameObject.transform.localScale = localScale;
             gameObject.layer = 10;
             gameObject.GetComponent<SpriteRenderer>().sprite = WarningSprite;
         }
         else
         {
+            float width = Camera.main.orthographicSize * Screen.width / Screen.height;
+            width = width / 2.6f;
+            float heigth = Camera.main.orthographicSize / 2.6f;
+            Vector3 localScale = gameObject.transform.localScale;
+            localScale.x = width * 2;
+            localScale.y = heigth * 0.4f;
+            gameObject.transform.localScale = localScale;
             tiempoDeDaño -= Time.deltaTime;
             gameObject.layer = 8;
             gameObject.GetComponent<SpriteRenderer>().sprite = correctSprite;
--- Minijuego/Assets/Scripts/Enemies/Laser Game/LaserScriptVertical.cs
+++ Minijuego/Assets/Scripts/Enemies/Laser Game/LaserScriptVertical.cs
@@ -11,14 +11,8 @@ public class LaserScriptVertical : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        float width = Camera.main.orthographicSize * Screen.width / Screen.height;
-        width = width / 2.6f;
-        float heigth = Camera.main.orthographicSize / 2.6f;
-        Vector3 localScale = gameObject.transform.localScale;
-        localScale.x = width * 1.4f;
-        localScale.y = heigth * 2;
-        gameObject.transform.localScale = localScale;
         correctSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0, gameObject.transform.position.z);
     }
 
     // Update is called once per frame
@@ -28,12 +22,26 @@ public class LaserScriptVertical : MonoBehaviour {
         {
             gameObject.layer = 10;
             gameObject.GetComponent<SpriteRenderer>().sprite = WarningSprite;
+            float width = Camera.main.orthographicSize * Screen.width / Screen.height;
+            width = width / 2.6f;
+            float heigth = Camera.main.orthographicSize / 2.6f;
+            Vector3 localScale = gameObject.transform.localScale;
+            localScale.x = width * 0.1f;
+            localScale.y = heigth * 0.12f;
+            gameObject.transform.localScale = localScale;
         }
         else
         {
             tiempoDeDaño -= Time.deltaTime;
             gameObject.layer = 8;
             gameObject.GetComponent<SpriteRenderer>().sprite = correctSprite;
+            float width = Camera.main.orthographicSize * Screen.width / Screen.height;
+            width = width / 2.6f;
+            float heigth = Camera.main.orthographicSize / 2.6f;
+            Vector3 localScale = gameObject.transform.localScale;
+            localScale.x = width * 1.4f;
+            localScale.y = heigth * 2;
+            gameObject.transform.localScale = localScale;
         }
         tiempoDeAdvertencia -= Time.deltaTime;
         if (tiempoDeDaño <= 0)
