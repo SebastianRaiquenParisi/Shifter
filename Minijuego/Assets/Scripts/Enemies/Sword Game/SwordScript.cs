1faf21ad945e678bc3c53b80cd2f1e7e6d7beb48
ParEasy <sraiquenp@gmail.com>
2017-06-16 00:21:47 -0300

    espada

---
+++ Minijuego/Assets/Scripts/Enemies/Sword Game/SwordScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour {
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (GameObject.Find("SuperAttackSword(Clone)") == null)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
