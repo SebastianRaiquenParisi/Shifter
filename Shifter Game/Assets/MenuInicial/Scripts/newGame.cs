84f4f8884bae9d0b95260b987d6cfb16f14df0f6
JoacoLerer <joacolerer@gmail.com>
2017-06-22 22:47:34 -0300

    Organizado

---
+++ Shifter Game/Assets/MenuInicial/Scripts/newGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class newGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadScene (int level) {
+        SceneManager.LoadScene(level);
+    }
+	
+}
